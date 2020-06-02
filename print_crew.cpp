91c4a38cb96e29cdc33d71df98d928155229ff54
znltnv <62938268+znltnv@users.noreply.github.com>
2020-06-02 20:04:26 +0300

    добавление функции чтения базы экипажей

--- print_crew.cpp
+++ print_crew.cpp
@@ -1,8 +1,35 @@
+#include<fstream>
+#include<iostream>
+#include<string>
 #include "all.h"
 #include "crew.h"
 #include "call.h"
 
+/*функция вывода базы экипажей,т.е/ФИО врача/ /ФИО фельдшера/ /ФИО водителя/....*/
+
 int print_crew(char * file_call, char * file_crew) {
+    ifstream fin;
+	fin.open(file_crew);
+	if (!(fin.is_open()))
+	{
+		cout << "Error file not open!!"<< endl;
+	}
+	int line_count=0;
+	string str;
+	while (getline(fin,str))    //подсчет количества строк в файле сrew 
+	{
+		line_count++;
+	}
+	fin.close();
+	fin.open(file_crew);
+	crew *crew_1 = new crew[line_count];
+	for (int i = 0; i < line_count; i++) 
+    {
+        fin >> crew_1[i];
+        cout << crew_1[i];
+    }
+	fin.close();
+	delete [] crew_1;
 
     ask(file_call, file_crew);
     return 0;
