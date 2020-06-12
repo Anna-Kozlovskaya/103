959e54f69f39ab28139f2a915a542846f809f425
znltnv <62938268+znltnv@users.noreply.github.com>
2020-06-12 18:22:27 +0300

    добавлено комментирование doxygen

--- all.h
+++ all.h
@@ -3,15 +3,74 @@
 
 
 //Здесь объявлены все функции.
-
-int menu(char * file_call, char * file_crew); //меню
-int print_crew(char * file_call, char * file_crew); //вывести список экипажей
-int print_call(char * file_call, char * file_crew); //вывести историю вызовов
-int add_crew(char * file_call, char * file_crew); //добавить экипаж
-int deletes_crew(char * file_call, char * file_crew); // удалить экипаж
-int set_crew(char * file_call, char * file_crew); //установить дежурные экипажи
-int print_duty_crew(char * file_call, char * file_crew); //вывести дежурные экипажи
-int add_call(char * file_call, char * file_crew); //принять вызов
-int set_time(char * file_call, char * file_crew); //добавить N часов
-void search(char * file_call, char * file_crew); //найти экипаж
+/*!
+	\brief Функция меню
+	\param file_call Текстовый документ с данными о вызовах
+	\param file_crew Текстовый документ с данными о экипажах
+		
+*/
+int menu(char * file_call, char * file_crew);
+/*!
+	\brief Функция вывода базы данных экипажей
+	\param file_call Текстовый документ с данными о вызовах
+	\param file_crew Текстовый документ с данными о экипажах
+	\return 0 если функция выполнена
+*/
+int print_crew(char * file_call, char * file_crew);
+/*!
+	\brief Функция вывода истории вызовов 
+	\param file_call Текстовый документ с данными о вызовах
+	\param file_crew Текстовый документ с данными о экипажах
+	\return 0 если функция выполнена
+*/
+int print_call(char * file_call, char * file_crew);
+/*!
+	\brief Функция добавления экипажей
+	\param file_call Текстовый документ с данными о вызовах
+	\param file_crew Текстовый документ с данными о экипажах
+	\return 0 если функция выполнена
+*/
+int add_crew(char * file_call, char * file_crew); 
+/*!
+	\brief Функция удаления экипажей
+	\param file_call Текстовый документ с данными о вызовах
+	\param file_crew Текстовый документ с данными о экипажах
+	\return 0 если функция выполнена
+*/
+int deletes_crew(char * file_call, char * file_crew);
+/*!
+	\brief Функция назначения дежурных экипажей
+	\param file_call Текстовый документ с данными о вызовах
+	\param file_crew Текстовый документ с данными о экипажах
+	\return 0 если функция выполнена
+*/
+int set_crew(char * file_call, char * file_crew); 
+/*!
+	\brief Функция вывода дежурных экипажей
+	\param file_call Текстовый документ с данными о вызовах
+	\param file_crew Текстовый документ с данными о экипажах
+	\return 0 если функция выполнена
+*/
+int print_duty_crew(char * file_call, char * file_crew);
+/*!
+	\brief Функция принятия вызовов
+	\param file_call Текстовый документ с данными о вызовах
+	\param file_crew Текстовый документ с данными о экипажах
+	\return 0 если функция выполнена
+*/
+int add_call(char * file_call, char * file_crew);
+/*!
+	\brief Функция времени
+	\param file_call Текстовый документ с данными о вызовах
+	\param file_crew Текстовый документ с данными о экипажах
+	\return 0 если функция выполнена
+*/
+int set_time(char * file_call, char * file_crew);
+/*!
+	\brief Функция поиска экипажей
+	\param file_call Текстовый документ с данными о вызовах
+	\param file_crew Текстовый документ с данными о экипажах
+  \return 0 если функция выполнена
+*/
+int search(char * file_call, char * file_crew);
 #endif // ALL_H
