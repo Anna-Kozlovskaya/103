4d8e18784a85979abc515ed96fd81debc40326eb
znltnv <62938268+znltnv@users.noreply.github.com>
2020-06-12 18:26:26 +0300

    добавление комментирования с помощью doxygen

--- call.h
+++ call.h
@@ -4,18 +4,29 @@
 #include <iostream>
 
 using namespace std;
-
+/*!
+	\brief  Класс для работы с вызовами 
+*/
 class call
 {
 private:
-    string address; //адрес (улица.номер дома.номер квартиры)
-    int time_to; //время до пациента (число часов)
-    string need_hosp; //необходимость госпитализации (есть, нет)
-    int time_back; //время до больницы, если осуществляется госпиализация (число часов)
-    string assigned_crew; //назначенный экипаж (фамилия врача)
-    string status; //статус вызова (выехали, выполнена госпитализация, завершен)
-
+    string address; ///< адрес (улица.номер дома.номер квартиры)
+    int time_to; ///< время до пациента (число часов)
+    string need_hosp; ///< необходимость госпитализации (есть, нет)
+    int time_back; ///< время до больницы, если осуществляется госпиализация (число часов)
+    string assigned_crew; ///< назначенный экипаж (фамилия врача)
+    string status; ///< статус вызова (выехали, выполнена госпитализация, завершен)
+    /*!
+			\brief Перегрузка операторов вывода
+			\param os Поток данных
+			\param call Данные вызовов
+	*/
     friend ostream& operator<< (ostream& os, const call& call);
+    /*!
+		   \brief Перегрузка операторов ввода
+		   \param is Поток данных
+		   \param call Данные вызовов
+   */
     friend istream& operator>> (istream& is, call& call);
 
     friend int print_call(char * file_call, char * file_crew);
