d2af0b73058c2abb11fcc9a7f82f8f0fceb5d325
znltnv <62938268+znltnv@users.noreply.github.com>
2020-06-12 18:30:33 +0300

    добавлено комментирование doxygen

--- crew.h
+++ crew.h
@@ -4,21 +4,33 @@
 #include <iostream>
 
 using namespace std;
-
+/*!
+	\brief  Класс для работы с базой данных экипажей
+*/
 class crew
 {
 private:
-    string doctor; //фамилия врача
-    string paramedic; //фамилия фельдшера
-    string driver; //фамилия водителя
-    int duty_status; //статус дежурства: число часов на дежурстве
-    int call_status; //статус вызова: когда освободится с текущего вызова
-    bool duty_bool;//	
-
+    string doctor;///< фамилия врача
+    string paramedic;///< фамилия фельдшера
+    string driver;///< фамилия водителя
+    int duty_status;///< статус дежурства: число часов на дежурстве
+    int call_status;///< статус вызова: когда освободится с текущего вызова
+    bool duty_bool;///< статус дежураства(на дежурстве/не на дежурстве)		
+    /*!
+			\brief Перегрузка операторов вывода
+			\param os Поток данных
+			\param crew Данные вызовов
+	*/
     friend ostream& operator<< (ostream& os, const crew& crew);
+    /*!
+		   \brief Перегрузка операторов ввода
+		   \param is Поток данных
+		   \param crew Данные вызовов
+   */
     friend istream& operator>> (istream& is, crew& crew);
 
     friend int print_crew(char * file_call, char * file_crew);
+    friend int search(char * file_call, char * file_crew);
     friend int add_crew(char * file_call, char * file_crew);
     friend int deletes_crew(char * file_call, char * file_crew);
     friend int set_crew(char * file_call, char * file_crew);
